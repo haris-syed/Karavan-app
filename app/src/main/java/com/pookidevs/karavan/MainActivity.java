48321824295211a3bd6661eaeb894beddb7fd767
Harry <syedharis632@gmail.com>
2020-02-27 16:35:30 +0500

    First commit

---
+++ app/src/main/java/com/pookidevs/karavan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.pookidevs.karavan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
